81225b20e84494977619306e5853af4ede6096f8
volosgoto <volosgoto@yahoo.com>
2018-07-30 14:56:40 +0300

    UI Vars

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert ('Hello');
+
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate resulrs
+function calculateResults(e) {
+    // console.log('Calculating');
+    // UI Variables
+    let amount = document.getElementById('amount');
+    let interest = document.getElementById('interest');
+    let years = document.getElementById('years');
+    let monthlyPayment = document.getElementById('monthly-payment');
+    let totalPayment = document.getElementById('total-payment');
+    let totalInterest = document.getElementById('total-interest');
+    
+    
+    
+    e.preventDefault();
+}
