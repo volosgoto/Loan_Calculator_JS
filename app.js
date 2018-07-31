08074a24280e04797d896bb51ea8ef420e53f4fd
andrey <volosgoto@yahoo.com>
2018-07-31 14:35:49 +0300

    add error handling

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
 
 // Calculate resulrs
 function calculateResults(e) {
-    
+
     // console.log('Calculating');
 
     // UI Variables
@@ -18,29 +25,38 @@ function calculateResults(e) {
     let principal = parseFloat(amount.value);
     let calculatedInterest = parseFloat(interest.value) / 100 / 12;
     let calculatedPayments = parseFloat(years.value) * 12;
-    
+
 
     // Monthly payments
-    let x  = Math.pow(1 + calculatedInterest, calculatedPayments);
+    let x = Math.pow(1 + calculatedInterest, calculatedPayments);
     // let monthly = ((principal * x * calculatedInterest) / (x - 1)).toFixed(2);
     let monthly = (principal * x * calculatedInterest) / (x - 1);
-    
+
     if (isFinite(monthly)) {
-        monthlyPayment.value = monthly.toFixed(2); 
+        monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        // Show Results
+        document.getElementById('results').style.display = 'block';
+        // Hide loader
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please check your numbers');
     }
 
-    e.preventDefault();
+    // e.preventDefault();
 
 }
 
 function showError(error) {
     // create Element
     let errorDiv = document.createElement('div');
-    
+    // Hide Results
+    document.getElementById('results').style.display = 'none';
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
     // add class
     errorDiv.className = 'alert alert-danger';
 
@@ -52,13 +68,13 @@ function showError(error) {
     let heading = document.querySelector('.heading');
 
     // Inser error above heading
-     card.insertBefore(errorDiv, heading);
+    card.insertBefore(errorDiv, heading);
 
-     // Clear error msg
-     setTimeout(clearError, 3000);
+    // Clear error msg
+    setTimeout(clearError, 3000);
 }
 
 function clearError(params) {
     document.querySelector('.alert').remove();
-    
+
 }
